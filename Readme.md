59502fabd0795dcfad2fae5ef41383c6c43025b6
Bappa Saha <bappasaha161@gmail.com>
2023-09-22 23:54:23 +0600

    [01] basic setup done

---
+++ Readme.md
@@ -0,0 +1,25 @@
+# First Start  redis in your mac:
+
+-   `brew services start redis`
+-   `redis-cli`
+-  `ping`
+
+### Hit the url to see results from your localhost
+
+`http://localhost:3001/publish`
+
+
+
+#   Install Commands
+
+-  `npm init -y `
+-  `npm i redis `
+-  `npm i express `
+
+# pub-sub-redis
+
+## Download Redis (Linux, Mac)
+https://redis.io/download/
+
+## Download Redis for Windows
+https://github.com/microsoftarchive/redis/releases
---
+++ pub.js
@@ -0,0 +1,55 @@
+const express =require('express');
+const redis =require('redis');
+
+const app =express();
+
+let publisherData =redis.createClient({
+    url: 'redis://localhost:6379'
+});
+
+publisherData.on('error',()=>console.log('Redis error'));
+publisherData.on('connect',()=>console.log("🔺Redis is connected"));
+
+const connect =async()=>{
+    await publisherData.connect();
+};
+
+connect();
+
+app.get('/', (req, res) => {
+    res.send({
+        message: "Publisher active from port 3001"
+    })
+});
+
+//todo: we publish it from redis
+app.get('/publish', async(req, res) => {
+    
+    const id =Math.floor(Math.random()*10);
+    // we want to publish this data
+    const data={
+        id, 
+        message:`Message from :${id} id`
+    };
+
+    //here data is published
+    await publisherData.publish('PData:',JSON.stringify(data));
+
+    //after publish we send a response
+    res.send({
+        Message:"PData is successfully published"
+    })
+      
+});
+
+
+
+
+
+
+
+// run the express app
+app.listen(3001,()=>{
+    console.log("Publisher server start on 3001");
+});
+
